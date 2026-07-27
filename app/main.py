c676520c9ed074c89046157580bf3475103075ba
Alexandr <travmagolovu1312@gmail.com>
2026-07-27 15:57:36 +0300

    flake8 ruless attempt

--- app/main.py
+++ app/main.py
@@ -8,13 +8,12 @@ def format_linter_error(error: dict) -> dict:
 
 def format_single_linter_file(file_path: str,
                               errors: list) -> dict:
-    return {
-        "errors": [format_linter_error(err) for err in errors],
-        "path": file_path,
-        "status": "failed" if errors else "passed"
-        }
-
+    return {"errors": [format_linter_error(err)
+                       for err in errors],
+            "path": file_path,
+            "status": "failed" if errors else "passed"}
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [format_single_linter_file(file_path, errors) for file_path, errors in linter_report.items()]
+    return [format_single_linter_file(file_path, errors)
+            for file_path, errors in linter_report.items()]
